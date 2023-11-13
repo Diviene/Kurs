6d1655f06608ff9c43073f0a8c8ba5165348c78a
d-evdokimov <d-evdokimov@ALKONA-PC046>
2023-11-13 16:31:59 +0300

    -slight changes

--- Kurs/Client/wwwroot/css/script.js
+++ Kurs/Client/wwwroot/css/script.js
@@ -11,3 +11,16 @@ window.hideSideNavAndModal = () => {
     sideNav.classList.remove('show');
     modal.classList.remove('showModal');
 };
+
+function scrollIntoHeader(header) {
+    document.getElementById('header').scrollIntoView();
+}
+function scrollIntoServices(services) {
+    document.getElementById('services').scrollIntoView();
+}
+function scrollIntoRooms(rooms) {
+    document.getElementById('rooms').scrollIntoView();
+}
+function scrollIntoCustomers(customers) {
+    document.getElementById('customers').scrollIntoView();
+}
