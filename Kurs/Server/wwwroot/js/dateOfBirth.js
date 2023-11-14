3a251b9eca0fa4b6e0c4fd31eab47ad15ac5ea09
d-evdokimov <d-evdokimov@ALKONA-PC046>
2023-11-14 15:07:50 +0300

    changed logout and removed javascript

--- Kurs/Server/wwwroot/js/dateOfBirth.js
+++ Kurs/Server/wwwroot/js/dateOfBirth.js
@@ -1,12 +1,12 @@
-document.getElementById('yourFormId').addEventListener('submit', function (event) {
-    var dateOfBirth = new Date(document.getElementById('dateofbirth').value);
-    var age = new Date().getFullYear() - dateOfBirth.getFullYear();
-    var m = new Date().getMonth() - dateOfBirth.getMonth();
-    if (m < 0 || (m === 0 && new Date().getDate() < dateOfBirth.getDate())) {
-        age--;
-    }
-    if (age < 18) {
-        alert('You must be at least 18 years old.');
-        event.preventDefault();
-    }
-});
+//document.getElementById('yourFormId').addEventListener('submit', function (event) {
+//    var dateOfBirth = new Date(document.getElementById('dateofbirth').value);
+//    var age = new Date().getFullYear() - dateOfBirth.getFullYear();
+//    var m = new Date().getMonth() - dateOfBirth.getMonth();
+//    if (m < 0 || (m === 0 && new Date().getDate() < dateOfBirth.getDate())) {
+//        age--;
+//    }
+//    if (age < 18) {
+//        alert('You must be at least 18 years old.');
+//        event.preventDefault();
+//    }
+//});
